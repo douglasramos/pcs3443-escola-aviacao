968cfd53c2459f77228290d140146355d265e906
Fábio Fusimoto Pires <fabiofusimoto.pires@hotmail.com>
2019-05-10 19:58:23 -0300

    Atualização para incluir o CRUD do Instrutor. Falta alterar a graduation_date e birth_date para o tipo data ao invés de string e pensar nas exceções de cada caso de uso

--- src/application/instructor/use_cases.py
+++ src/application/instructor/use_cases.py
@@ -3,28 +3,41 @@ from core.instructor.instructor import Instructor
 
 from flask import request
 from pony.orm import *
+from pony.orm.serialization import to_dict
 
-
-def get_instructors_list():
+def get_instructors_list(): # retorna uma lista de dicts com informações de cada instrutor
     with db_session:
-        return{
-            {
-                'ID': instructor.ID,
-                'Name': instructor.name,
-                'License Number': instructor.license_number,
-                'Address': instructor.address,
-                'Birth Date': instructor.birth_date,
-                'Course': instructor.course_name,
-                'Graduation Date': instructor.graduation_date,
-                'Institution': instructor.institution
-            }
-            # essa sintaxe (com select, sem argumentos adicionais) percorre todos os items do tipo db_Tarefa
-            for instructor in Instructor.select()
-        }
+        instructor_list = [] # lista de instrutores
+        for instr in Instructor.select(): # essa sintaxe (com select, sem argumentos adicionais) percorre todos os elementos do tipo Instructor
+            instr_dict = to_dict(instr)
+            instructor_list.append(instr_dict)
+        return instructor_list
 
-def insert_new_instructor(ID : int, name: str, license_number : int, address : str, birth_date : str, 
+def insert_new_instructor(name: str, license_number : int, address : str, birth_date : str, 
                             course_name : str, graduation_date : str, institution : str):
     with db_session:
-        Instructor(ID = ID, name= name, license_number = license_number, address = address, 
+        Instructor(name= name, license_number = license_number, address = address, 
                     birth_date = birth_date, course_name = course_name, graduation_date = graduation_date,
                     institution = institution)
+        return ("Novo instrutor adicionado com sucesso")
+
+def delete_instructor(ID : int):
+    with db_session:
+        try:
+            instr = Instructor[ID]
+            if instr:
+                instr.delete()
+                return 'Instrutor removido com sucesso!'
+        except ObjectNotFound:
+                return 'Instrutor não encontrado'
+
+def alter_instructor(**args): # nem sempre deseja-se alterar todos os parâmetros, por isso uso o dict **args
+    with db_session:
+        # o args só vai conter os campos que eu desejo alterar
+        ID = args['ID']
+        try: # verifica-se, inicialmente se o instrutor consta no BD
+            instr = Instructor[ID]
+        except ObjectNotFound:
+            return 'Instrutor não encontrado'
+        instr.set(**args) # altera somente os argumentos cujas chaves estão explícitas no args
+        return ("Informações do instrutor alteradas com sucesso")
--- src/core/instructor/instructor.py
+++ src/core/instructor/instructor.py
@@ -2,11 +2,11 @@ from pony.orm import *
 from persistance.persistance import db
 
 class Instructor(db.Entity):
-    ID = PrimaryKey(int)
+    ID = PrimaryKey(int, auto=True) # o ID será gerado automaticamente
     name = Required(str)
-    license_number = Required(int)  #número do brevê
+    license_number = Required(int)  # número do brevê
     address = Required(str)
-    birth_date = Required(str) #depois temos que ver como faz para se usar date ao invés de string
+    birth_date = Required(str) # depois temos que ver como faz para se usar date ao invés de string
     course_name = Required(str)
-    graduation_date = Required(str)  #data de obtenção do diploma. Depois temos que ver como faz para se usar date ao invés de string
+    graduation_date = Required(str)  # data de obtenção do diploma. Depois temos que ver como faz para se usar date ao invés de string
     institution = Required(str)
