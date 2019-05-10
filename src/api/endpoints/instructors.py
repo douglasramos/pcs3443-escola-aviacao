025891fe901377f406a89bde77f2cbcc9911132d
Fábio Fusimoto Pires <fabiofusimoto.pires@hotmail.com>
2019-05-10 19:58:23 -0300

    Atualização para incluir o CRUD do Instrutor. Falta alterar a graduation_date e birth_date para o tipo data ao invés de string e pensar nas exceções de cada caso de uso

--- src/api/endpoints/instructors.py
+++ src/api/endpoints/instructors.py
@@ -1,11 +1,10 @@
 from flask import request
 from flask_restplus import Resource
 from api.api import api
-from application.instructor.use_cases import *
+from application.instructor.use_cases import insert_new_instructor, delete_instructor, get_instructors_list, alter_instructor
 
 ns = api.namespace(
     'instructors', description='Operations related to instructor CRUD')
-
 
 @ns.route('/')
 class instructorList(Resource):
@@ -16,7 +15,6 @@ class instructorList(Resource):
 
     def post(self):
         json_data = request.get_json(force = True)
-        ID = json_data['ID']
         name = json_data['name']
         license_number = json_data['license_number']
         address = json_data['address']
@@ -24,8 +22,17 @@ class instructorList(Resource):
         course_name = json_data['course_name']
         graduation_date = json_data['graduation_date']
         institution = json_data['institution']
-        return insert_new_instructor(ID, name, license_number, address, birth_date,
+        return insert_new_instructor(name, license_number, address, birth_date,
                                     course_name, graduation_date, institution)
+
+    def delete(self): # assume-se que o ID do instrutor a ser removido seja conhecido
+        json_data = request.get_json(force = True)
+        ID = json_data['ID']
+        return delete_instructor(ID)
+
+    def put(self): # ID é o único argumento obrigatório para um requisição de PUT
+        json_data = request.get_json(force = True)        
+        return alter_instructor(**json_data)        
 
 # @ns.route('/<int:id>')
 # @api.response(404, 'Instructor not found.')
--- src/persistance/persistance.py
+++ src/persistance/persistance.py
@@ -4,5 +4,5 @@ from pony.orm.serialization import to_dict
 db = Database()
 
 def initialize_database():
-    db.bind(provider = 'postgres', user = 'postgres', password = 'fabio2306@', host = 'localhost', database = 'postgres')
+    db.bind(provider = 'postgres', user = 'postgres', password = 'password', host = 'localhost', database = 'escola_aviacao')
     db.generate_mapping(create_tables = True)
