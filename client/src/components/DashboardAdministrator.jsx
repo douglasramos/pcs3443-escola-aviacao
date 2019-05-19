0a7b4fd66d4626d3c646071c2beb14681ece2bb8
Douglas Ramos <douglas.dcr@outlook.com>
2019-05-19 04:14:49 -0300

    Separação do forms para registrar aluno e instrutor. Melhorias no design do forms. Inclusão do botão cancelar (que limpa o forms). Implementação de responsividade do forms e da navbar.

--- client/src/components/DashboardAdministrator.jsx
+++ client/src/components/DashboardAdministrator.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
-import './NavBar.css';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 import RegisterInstructor from './RegisterInstructor';
-// import { Link } from 'react-router-dom';
+import RegisterStudent from './RegisterStudent';
 
-function DashboardAministrator() {
-  return (
-    <div>
-      <h3>Dashboard Administrador</h3>
-      <RegisterInstructor />
-    </div>
-  );
+const registerType = {
+  student: 1,
+  instructor: 2,
+};
+
+class DashboardAministrator extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      registerType: null,
+    };
+  }
+
+  changeTypeRegister = type => {
+    this.setState({ registerType: type });
+  };
+
+  render() {
+    return (
+      <div className="container mt-3">
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.changeTypeRegister(registerType.student)}
+          >
+            Aluno
+          </Button>
+          <Button
+            className="ml-3"
+            variant="contained"
+            style={{ backgroundColor: '#2cad58', color: 'white' }}
+            onClick={() => this.changeTypeRegister(registerType.instructor)}
+          >
+            Instrutor
+          </Button>
+        </div>
+        <div className="mt-5">
+          {this.state.registerType === registerType.instructor ? (
+            <RegisterInstructor />
+          ) : (
+            <RegisterStudent />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DashboardAministrator;
