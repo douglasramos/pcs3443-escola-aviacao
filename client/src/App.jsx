0d1c5d904b97767cc3c240cda75e39157b934fce
Douglas Ramos <douglas.dcr@outlook.com>
2019-05-19 20:59:31 -0300

    Melhoria do design do DashboardAdministrador, preparando melhor para a inclusão final de todos os componentes. Melhoria do código da navBar. Criação de um componente HomeApp (que atualmente redireciona para o dashboard do Administrador)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar';
 import DashboardInstructor from './components/DashboardIntructor';
 import DashboardAdministrator from './components/DashboardAdministrator';
 import DashboardStudent from './components/DashboardStudent';
+import HomeApp from './components/HomeApp';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Router>
         <div>
           <NavBar />
-          <Route exact path="/" component={DashboardAdministrator} />
+          <Route exact path="/" component={HomeApp} />
           <Route path="/dashboard-instructor" component={DashboardInstructor} />
           <Route path="/dashboard-administrator" component={DashboardAdministrator} />
           <Route path="/dashboard-student" component={DashboardStudent} />
