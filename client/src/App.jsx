ff6fb4364819c7120d2ad8b5c7f672c0b4e0c9bc
Fábio Fusimoto Pires <fabiofusimoto.pires@hotmail.com>
2019-05-23 10:40:44 -0300

    Inclusão de uma interface para agendar aulas e integração da mesma no dashboard do aluno

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,7 @@ import DashboardInstructor from './components/DashboardIntructor';
 import DashboardAdministrator from './components/DashboardAdministrator';
 import DashboardStudent from './components/DashboardStudent';
 import HomeApp from './components/HomeApp';
-import EditInstructor from './components/EditInstructor';
-import EditStudent from './components/EditStudent';
+import ScheduleLesson from './components/ScheduleLesson';
 
 function App() {
   return (
@@ -21,8 +20,7 @@ function App() {
           <Route path="/dashboard-instructor" component={DashboardInstructor} />
           <Route path="/dashboard-administrator" component={DashboardAdministrator} />
           <Route path="/dashboard-student" component={DashboardStudent} />
-          <Route path="/edit-instructor" component={EditInstructor} />
-          <Route path="/edit-student" component={EditStudent} />
+          <Route path="/schedule-lesson" component={ScheduleLesson} />
         </div>
       </Router>
     </React.Fragment>
