a39aac5bd2ee97ab67c52e49a75ddcb34e03ce95
Douglas Ramos <douglas.dcr@outlook.com>
2019-06-12 22:31:32 -0300

    deploy front no github

---
+++ docs/precache-manifest.0e7548d30ed2145c7cb2fd09d1d6da97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df33bb09fa710e5c2ee603857aacb46b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b8f389f8db19b70772",
+    "url": "/static/css/main.35e179ed.chunk.css"
+  },
+  {
+    "revision": "f8865533efc2373ad157",
+    "url": "/static/js/2.d76dda83.chunk.js"
+  },
+  {
+    "revision": "a5b8f389f8db19b70772",
+    "url": "/static/js/main.9fa55bd5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
